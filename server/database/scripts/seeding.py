888a06fb307367d06e926771d92513611c273cdb
OfficeCoffee <rosetaylor45342@gmail.com>
2026-02-06 18:32:06 -0500

    Created util methods in seeding.py

--- server/database/scripts/seeding.py
+++ server/database/scripts/seeding.py
@@ -1,3 +1,26 @@
 import pandas as pd
+from prisma import Prisma
+import asyncio
+
+CSV_PATH = 'yeah_man.csv'
+BATCH_SIZE = 500
+
+def normalize_value(value):
+    
+    # For you goobers, pandas generates a NaN for missing values and Prisma wants None
+    if pd.isna(value):
+        return None
+    
+    # Convert any numby scalars to python standard types
+    if hasattr(value, "item"):
+        return value.item()
+    
+    return value\
+
+def row_to_dict(row):
+
+
+df = pd.read_csv(CSV_PATH)
+data_to_insert = df.to_dict(orient='records')
 
 print("Hello, World!") 
