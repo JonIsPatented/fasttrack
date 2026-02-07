ec1027a78ee979ff1c9d2ce90281abc11c029f5b
Jon Cole <jonispatented@gmail.com>
2026-02-06 20:57:40 -0500

    implemented endpoint (maybe)

--- server/src/routes/coasters.ts
+++ server/src/routes/coasters.ts
@@ -1,9 +1,24 @@
 import { Router, type Request, type Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import Database from 'better-sqlite3'
+
 
 const router = Router()
 
-router.get('/', (req: Request, res: Response) => {
-    res.send('Coasters go here')
+const path = '../database/database.db'
+const db = new Database(path)
+const adapter = new PrismaBetterSqlite3({ url: path })
+
+const prisma = new PrismaClient({ adapter })
+
+router.get('/', async (req: Request, res: Response) => {
+    try {
+        const coasters = await prisma.coaster.findMany()
+        res.json(coasters)
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong fetching coasters' })
+    }
 })
 
 export default router
