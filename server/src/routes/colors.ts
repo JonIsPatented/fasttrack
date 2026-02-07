62c406d9132f4472704117eede01102218930cc4
Jon Cole <jonispatented@gmail.com>
2026-02-07 11:26:32 -0500

    added colors route

---
+++ server/src/routes/colors.ts
@@ -0,0 +1,64 @@
+import { Router, type Request, type Response } from 'express'
+import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import Database from 'better-sqlite3'
+
+
+const router = Router()
+
+const path = '../database/database.db'
+const db = new Database(path)
+const adapter = new PrismaBetterSqlite3({ url: path })
+
+const prisma = new PrismaClient({ adapter })
+
+router.get('/get', async (req: Request<{}, {}, {}, { verbose: boolean, id: number }>, res: Response) => {
+    const verboseCoasterInclude = {
+        include: {
+            coaster: {
+                include: {
+                    manufacturer: true,
+                    material: true,
+                    home_park: true,
+                    track_colors: {
+                        include: {
+                            color: true
+                        }
+                    },
+                    support_colors: {
+                        include: {
+                            color: true
+                        }
+                    },
+                    coasterTraits: {
+                        include: {
+                            trait_type: true
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const { verbose, id } = req.query
+
+    let where: any = {}
+
+    if (id !== null) where.color_id = id
+
+    try {
+        const colors = await prisma.color.findMany({
+            include: {
+                coaster_supports: verbose ? verboseCoasterInclude : true,
+                coaster_tracks: verbose ? verboseCoasterInclude : true
+            },
+            where
+        })
+        res.json(colors)
+    } catch (error) {
+        res.status(500).json({ error: 'Something went wrong fetching coasters' })
+    }
+})
+
+export default router
+
