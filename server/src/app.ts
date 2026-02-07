749fea26de93542049f8a843e1a8f702b8b61993
Jon Cole <jonispatented@gmail.com>
2026-02-06 19:51:14 -0500

    Added routing (kinda)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,13 @@
-import express, { Request, Response } from 'express';
+import express from 'express'
+import coasterRouter from './routes/coasters.ts'
 
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, Express with TypeScript!');
-});
+app.use('/api/coasters', coasterRouter)
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
