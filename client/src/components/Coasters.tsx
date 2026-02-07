af15c96961ffba22256326adb7253bc8942104c3
Wamski <waskyjo@gmail.com>
2026-02-07 09:52:47 -0500

    Coasters Page CSS

--- client/src/components/Coasters.tsx
+++ client/src/components/Coasters.tsx
@@ -7,25 +7,28 @@ function Coasters (){
   const { data, isLoading, error } = useQuery({
     queryKey: ['coasters'],
     // We use a relative path now!
-    queryFn: () => fetch('/api/coaster/get').then(res => {
+    queryFn: () => fetch('/api/coasters/').then(res => {
       if (!res.ok) throw new Error('Network response was not ok');
       return res.json();
     }),
   });
 
   let coasterElement = data?.map((coaster) => (
-    <p>{coaster}</p>
+    <li>{coaster}</li>
 ));
 
-  if (isLoading) coasterElement = <p>Loading...</p>;
-  if (error) coasterElement =  <p>Error: {error.message}</p>; 
+  if (isLoading) coasterElement = <li>Loading...</li>;
+  if (error) coasterElement =  <li>Error: {error.message}</li>; 
   return (
     <section className="Coasters" id="Coasters">
       <div>
         <img src={CSplash} alt="CoasterSpash"></img>
       </div>
       <div className="CoastersContent">
-        {coasterElement}
+        <ul>
+          {coasterElement}
+
+        </ul>
       </div>
     </section>
   )
